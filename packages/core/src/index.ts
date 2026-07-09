80eef6e6a3e55bccefd3d9a46e8084e8726b1c4d
maki maki <chenjunhong54321@163.com>
2026-07-10 00:57:12 +0800

    chore: release 0.7.0-beta.1

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,7 +4,7 @@
  * Public API exports.
  */
 
-export const VERSION = "0.6.0-rc.18";
+export const VERSION = "0.7.0-beta.1";
 
 // ─── Types ───────────────────────────────────────────────────────
 
